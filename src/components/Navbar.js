e1c8d3302342249fd694b8ac3e349050860a3c9d
Christian Carestia <christian@data-lens.co.uk>
2021-01-26 12:03:15 +0000

    ONTOLOGY-54: Removed unused icon from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,9 @@ import React from 'react'
 import { connect } from 'redux-zero/react'
 import { useTranslation } from 'react-i18next'
 import PropTypes from 'prop-types'
-import {
-  BsSearch,
-} from 'react-icons/bs'
 import actions from '../store/actions'
 
 const Navbar = ({
-  setStoreState,
-  isSearchOpen,
   availableNodesNormalised,
   availableEdgesNormalised
 }) => {
@@ -22,34 +17,19 @@ const Navbar = ({
           {`${t('nodes')}: ${Object.keys(availableNodesNormalised).length} | ${t('edges')}: ${Object.keys(availableEdgesNormalised).length}`}
         </span>
       </div>
-
-      <div className="nav-right">
-        <button
-          type="button"
-          title={t('search')}
-          className={isSearchOpen ? 'nav-right-button-selected' : ''}
-          onClick={() => setStoreState('isSearchOpen', !isSearchOpen)}
-        >
-          <BsSearch />
-        </button>
-      </div>
     </nav>
   )
 }
 
 Navbar.propTypes = {
-  isSearchOpen: PropTypes.bool.isRequired,
-  setStoreState: PropTypes.func.isRequired,
   availableNodesNormalised: PropTypes.shape().isRequired,
   availableEdgesNormalised: PropTypes.shape().isRequired,
 }
 
 const mapToProps = ({
-  isSearchOpen,
   availableNodesNormalised,
   availableEdgesNormalised
 }) => ({
-  isSearchOpen,
   availableNodesNormalised,
   availableEdgesNormalised
 })
