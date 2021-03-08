de047ebc341958584ce31aa8f51aa043f4f87191
Christian Carestia <carestia@engiweb.com>
2021-03-08 16:51:21 +0000

    ONTOLOGY-110 Turned search into form to allow pressing enter on search

--- next.config.js
+++ next.config.js
@@ -1,29 +1,12 @@
 const withImages = require('next-images')
-// const WorkerPlugin = require('worker-plugin')
 
 module.exports = withImages({
   target: 'serverless',
-  // future: {
-  //   webpack5: true
-  // },
   webpack: (config, {
     webpack,
-    // isServer
   }) => {
-    // if (!isServer) {
-    //   config.plugins.push(
-    //     new WorkerPlugin({
-    //       // use "self" as the global object when receiving hot updates.
-    //       globalObject: 'self',
-    //     })
-    //   )
-    // }
     config.plugins.push(new webpack.IgnorePlugin(/\/__test__\//))
-    // config.plugins.push(
-    //   new webpack.DefinePlugin({
-    //     __CLIENT__: true,
-    //   })
-    // )
+
     return config
   },
 })
