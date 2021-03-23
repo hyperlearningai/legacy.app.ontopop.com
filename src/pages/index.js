061e4c96f1f74e3aecfaf239baacda64bae60d74
michal kostyal <michalkostyal@gmail.com>
2021-03-23 23:25:42 +0000

    Add mouseflow and userback

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { connect } from 'redux-zero/react'
 import PropTypes from 'prop-types'
+import InnerHTML from 'dangerously-set-html-content'
 import Navbar from '../components/Navbar'
 import FooterComponent from '../components/FooterComponent'
 import HeadTags from '../components/HeadTags'
@@ -18,6 +19,27 @@ const Index = ({
   user,
   setStoreState
 }) => {
+  const customHtml = `
+    <script>
+        window.Userback = window.Userback || {};
+        Userback.access_token = '29230|42198|CVJlhtfkaYN3J3xP2x200ghJ1';
+        (function(d) {
+            var s = d.createElement('script');s.async = true;
+            s.src = 'https://static.userback.io/widget/v1.js';
+            (d.head || d.body).appendChild(s);
+        })(document);
+    </script>
+    <script>
+      window._mfq = window._mfq || [];
+      (function() {
+        var mf = document.createElement("script");
+        mf.type = "text/javascript"; mf.defer = true;
+        mf.src = "//cdn.mouseflow.com/projects/17067ed4-6a87-41f9-b1f0-44249c9c1bfe.js";
+        document.getElementsByTagName("head")[0].appendChild(mf);
+      })();
+    </script>
+  `
+
   const { t } = useTranslation()
 
   const router = useRouter()
@@ -43,6 +65,8 @@ const Index = ({
         title=""
         description={t('ontologyVisualisationDescription')}
       />
+
+      {process.env.NEXT_PUBLIC_ADD_FEEDBACK === 'true' && <InnerHTML html={customHtml} />}
 
       {
         (user.email !== ''
