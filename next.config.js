78f475cd97e258d7cce8d947b5adebf8685dee86
Christian Carestia <carestia@engiweb.com>
2021-04-07 17:53:42 +0100

    ONTOLOGY-133 Updated next config

--- next.config.js
+++ next.config.js
@@ -1,57 +1,13 @@
 const withImages = require('next-images')
 
 module.exports = withImages({
-  target: 'serverless', // 'serverless',
-  // trailingSlash: false, // true,
-  redirects: async () => [
-    {
-      source: '/_error',
-      destination: '/search',
-      permanent: false
-    },
-  ],
+  target: 'serverless',
   exportPathMap: () => {
-    // const dynamicRoutes = [
-    //   '/search',
-    //   // '/network-graphs',
-    //   // '/network-graph-options',
-    //   // '/free-text-search',
-    //   // '/structured-search',
-    //   // '/nodes-selection',
-    //   // '/edges-selection',
-    //   // '/nodes-filter',
-    //   // '/edges-filter',
-    //   // '/bounding-box',
-    //   // '/node-neighbourhood',
-    //   // '/shortest-path',
-    //   // '/settings',
-    //   // '/styling',
-    //   // '/notes',
-    //   // '/synonyms',
-    //   // '/export',
-    //   // '/custom-query',
-    //   // '/edit-ontology'
-    // ]
-
     const routes = {
       '/login': { page: '/login' },
       '/profile': { page: '/profile' },
       '/': { page: '/' },
-      // '/app/:slug': { page: '/app/[...slug]' }
     }
-
-    // dynamicRoutes.forEach((route) => {
-    //   // const view = route.replace('/', '')
-
-    //   routes[`${route}`] = {
-    //       page: 'route'
-    //   }
-
-    //   // routes[`/app${route}`] = {
-    //   //   page: '/app/[view]',
-    //   //   query: { view }
-    //   // }
-    // })
 
     return routes
   },
